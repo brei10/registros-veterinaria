7c5e5cd972a98bc165b53e3fadbeed0606a14f1e
breiner uribe <uribebreiner971@gmail.com>
2022-10-15 18:15:50 -0500

    segundo commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let registroEntradas = JSON.parse("registros") ?? [];
+let registroEntradas = JSON.parse(localStorage.getItem("registros")) ?? [];
+/* si ya hay elementos en el local storage se llama la funcion crear tarjeta*/
 if (registroEntradas.length > 0) {
-    crearTarjeta(registroEntradas);
-
+    registroEntradas.forEach((o)=>{
+        crearTarjeta(o);
+    })
+}
+/*si ya hay elemento en local storage se agrega en tabla*/
+let registroSalidas = JSON.parse(localStorage.getItem("salida")) ?? [];
+console.log(registroSalidas)
+if(registroSalidas.length>0){
+    crearTablaSalida()
 }
 
-let registroSalidas = [];
-
+// obtener formulario mas evento
 let btnForm = document.querySelector("#btnForm");
 btnForm.addEventListener("click", (e) => {
     e.preventDefault();
@@ -26,11 +33,10 @@ function registrarEntradas(propietario, sintomas, fecha, mascota) {
         id: Math.random(),
     }
     registroEntradas.push(objeto);
-    crearTarjeta();
+    crearTarjeta(objeto);
 }
 
-function crearTarjeta() {
-    registroEntradas.forEach((objeto) => {
+function crearTarjeta(objeto) {
         let tarjeta = ``;
         tarjeta = `
             <div id="${objeto.id}" class="contenido">
@@ -43,42 +49,54 @@ function crearTarjeta() {
             </div>
         `
         document.querySelector("#padreTarjetas").insertAdjacentHTML("afterbegin", tarjeta);
-    })
-
+        saveStorage()
 }
-
+    // esta funcion compara el id seleccionado con el de la base de datos
+    // pushea el objeto y se agrega al arreglo para posteriormente agregarlo al storage
 function registrarSalida(id) {
     registroEntradas.filter((o) => {
         if (o.id == id) {
-            crearTablaSalida(o);
-
+            o.fechaSalida = new Date(); // agregamos la fecha de salida
+            registroSalidas.push(o);
+            localStorage.setItem("salida",JSON.stringify(registroSalidas))
+            crearTablaSalida();
         };
     })
-    // encontrar el elemento y pasarlo a otro arreglo
-    registroEntradas = registroEntradas.filter((o) => {
+    // esto es para borrar del arreglo al objeto que vamos a sacar de la tarjeta
+        registroEntradas = registroEntradas.filter((o) => {
         return o.id !== id;
 
     })
+    // eliminamos el local storage para remplazarlo sin el objeto sacado
+    localStorage.removeItem("registros")
+    // actualizamos
+    localStorage.setItem("registros",JSON.stringify(registroEntradas))
 
     // eliminar de la tabla
     let tarjeta = document.getElementById(id);
     let padre = tarjeta.parentElement;
     padre.removeChild(tarjeta);
-
-
 }
 
-function crearTablaSalida(d) {
-    let fecha = new Date();
-
+function crearTablaSalida() {
+    let d = []
+     d = JSON.parse(localStorage.getItem("salida"))
+     d.forEach((d)=>{    
+         console.log(d)
+            
     let tabla = ``;
     tabla = `
         <tr id="${d.id}"></tr>
         <td>${d.propietario}</td>
         <td>${d.mascota}</td>
         <td>${d.sintomas}</td>
-        <td>${fecha}</td>
-
+        <td>${d.fechaSalida}</td>
         `;
-    document.querySelector("#padreTablaSalida").insertAdjacentHTML("beforeend", tabla);
+    document.querySelector("#padreTablaSalida").insertAdjacentHTML("afterbegin", tabla);
+   
+}) 
+     }
+
+function saveStorage(){
+ localStorage.setItem("registros",JSON.stringify(registroEntradas));
 }
